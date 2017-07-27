c297f29ad7fe33644ab0584614d8d9b336c9de35
gwarner3 <george@georgedevelops.com>
2017-07-27 09:19:12 -0500

    Not committing this

--- itunesSearch.js
+++ itunesSearch.js
@@ -3,10 +3,9 @@
 function searchItunes(){
 	var searchInput = document.getElementById("searchBar").value;
 	//Do something if input has spaces
-	
+
 	var searchLink = "https://itunes.apple.com/search?term=" + searchInput + "&limit=5";
 
-	
 	$.getJSON(searchLink, function(data){
 		console.log(data);
 	});
